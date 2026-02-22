7ea93261702278fc285d2c4146fc81d9b230b64c
SnowFlash383935 <snowflash383935@mail.ru>
2026-02-22 15:46:10 +0300

    Add asm_reverse function and update main logic

--- main.c
+++ main.c
@@ -2,15 +2,18 @@
 
 // Объявляем внешнюю функцию из ассемблера
 extern long long asm_strlen(const char* str);
+extern char* asm_reverse(const char* str);
 
 int main() {
     const char* test_str = "GitHub Actions on ARM64!";
     long long len = asm_strlen(test_str);
+    const char* reversed_str = asm_reverse(test_str);
     
     printf("String: %s\n", test_str);
     printf("Detected length: %lld\n", len);
+    printf("Reversed string: %s\n", reversed_str);
     
     // Простейший unit-test: если длина не 24, выходим с ошибкой
-    if (len == 24) return 0;
+    if (len == 24 & reversed_str == "!46MRA no snoitcA buHtiG") return 0;
     return 1;
 }
