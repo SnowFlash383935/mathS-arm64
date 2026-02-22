2be75b6d6ca898b23f1f7d4bc7be8b0efc72f4e7
SnowFlash383935 <snowflash383935@mail.ru>
2026-02-22 16:53:53 +0300

    Add vector_hypot function for vectorized Pythagoras

--- fast_module.c
+++ fast_module.c
@@ -1,30 +1,36 @@
-#define PY_SSIZE_T_CLEAN
 #include <Python.h>
 
-extern void asm_reverse(char* str);
+extern void vector_hypot(float* a, float* b, float* out, int n);
 
-static PyObject* method_reverse(PyObject* self, PyObject* args) {
-    char* str;
-    if (!PyArg_ParseTuple(args, "s", &str)) return NULL;
-
-    // Дублируем строку, так как строки в Python неизменяемы
-    char* buffer = strdup(str);
-    asm_reverse(buffer);
+static PyObject* method_vector_hypot(PyObject* self, PyObject* args) {
+    Py_buffer a_view, b_view, out_view;
     
-    PyObject* result = Py_BuildValue("s", buffer);
-    free(buffer);
-    return result;
+    // Принимаем три буфера (A, B и выходной массив)
+    if (!PyArg_ParseTuple(args, "y*y*y*", &a_view, &b_view, &out_view)) {
+        return NULL;
+    }
+
+    int n = a_view.len / sizeof(float);
+    
+    // Вызываем наш ассемблер
+    vector_hypot((float*)a_view.buf, (float*)b_view.buf, (float*)out_view.buf, n);
+
+    PyBuffer_Release(&a_view);
+    PyBuffer_Release(&b_view);
+    PyBuffer_Release(&out_view);
+    
+    Py_RETURN_NONE;
 }
 
-static PyMethodDef FastMethods[] = {
-    {"reverse", method_reverse, METH_VARARGS, "Reverse a string using ARM64 ASM"},
+static PyMethodDef MathSMethods[] = {
+    {"vector_hypot", method_vector_hypot, METH_VARARGS, "Vectorized Pythagoras"},
     {NULL, NULL, 0, NULL}
 };
 
-static struct PyModuleDef fastmodule = {
-    PyModuleDef_HEAD_INIT, "fast_arm", NULL, -1, FastMethods
+static struct PyModuleDef maths_module = {
+    PyModuleDef_HEAD_INIT, "mathS", NULL, -1, MathSMethods
 };
 
-PyMODINIT_FUNC PyInit_fast_arm(void) {
-    return PyModule_Create(&fastmodule);
+PyMODINIT_FUNC PyInit_mathS(void) {
+    return PyModule_Create(&maths_module);
 }
