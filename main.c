f5066838e5913e258e48a464ce5baee04d889a01
SnowFlash383935 <snowflash383935@mail.ru>
2026-02-22 15:50:32 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
+#include <string.h>
 
 // Объявляем внешнюю функцию из ассемблера
 extern long long asm_strlen(const char* str);
 extern char* asm_reverse(const char* str);
 
 int main() {
-    const char* test_str = "GitHub Actions on ARM64!";
+    // Используем [] вместо *, чтобы создать массив в стеке
+    char test_str[] = "GitHub Actions on ARM64!"; 
+    
     long long len = asm_strlen(test_str);
-    const char* reversed_str = asm_reverse(test_str);
+    
+    // Передаем массив в функцию реверса
+    asm_reverse(test_str); 
     
     printf("String: %s\n", test_str);
     printf("Detected length: %lld\n", len);
-    printf("Reversed string: %s\n", reversed_str);
     
-    // Простейший unit-test: если длина не 24, выходим с ошибкой
-    if (len == 24 & reversed_str == "!46MRA no snoitcA buHtiG") return 0;
+    // Сравнение строк в Си делается через strcmp, а не ==
+    if (len == 24 && strcmp(test_str, "!46MRA no snoitcA buHtiG") == 0) return 0;
     return 1;
 }
